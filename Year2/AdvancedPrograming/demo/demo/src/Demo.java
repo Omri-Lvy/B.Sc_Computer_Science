b7dc6f618f4a29f27e9e6565d0586d4b78ad1933
Omri Levy <79089193+Omri-Lvy@users.noreply.github.com>
2024-01-29 17:38:23 +0200

    Delete Year2/AdvancedPrograming directory

--- Year2/AdvancedPrograming/demo/demo/src/Demo.java
+++
@@ -1,3 +0,0 @@
-public class Demo {
-    Integer a = 6;
-}
--- Year2/AdvancedPrograming/demo/demo/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String[] args){
-        Student student = new Student("Noa",23.0,10.0,12345);
-        System.out.println(student.getName());
-        System.out.println(student.getAge());
-        System.out.println(student.getId());
-    }
-}
